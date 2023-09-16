e1635954169307ddaa263f2b6852820d0d41fae3
Bruno <brunojkassab@gmail.com>
2023-09-15 22:42:16 -0300

    Tela de logi, validação do campo email.

--- app/src/main/java/br/com/fiap/myapplication/ui/screen/LoginScreen.kt
+++ app/src/main/java/br/com/fiap/myapplication/ui/screen/LoginScreen.kt
@@ -2,6 +2,7 @@
 
 package br.com.fiap.myapplication.ui.screen
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Box
 //import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.foundation.layout.Column
@@ -11,15 +12,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import br.com.fiap.myapplication.R
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.getValue
@@ -28,13 +37,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -49,13 +61,26 @@ fun LoginScreen() {
     var password by remember {
         mutableStateOf("")
     }
+
+    var emailError by remember {
+        mutableStateOf(false)
+    }
+
+    var isFocused by remember {
+        mutableStateOf(false)
+    }
+
+    //val textColor = if (isFocused) Color.Green else Color.Black
+
+    val tamanhoMaximo = 8
+
     Box() {
         Column(modifier = Modifier.padding(top = 80.dp)) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(170.dp)
+                    .height(190.dp)
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.logotechmed),
@@ -65,73 +90,94 @@ fun LoginScreen() {
                         .padding(top = 16.dp)
                 )
             }
-            Spacer(modifier = Modifier.padding(16.dp))
-            var erroEmail by remember {
-                mutableStateOf(false)
+            //Spacer(modifier = Modifier.padding(16.dp))
+            Column(
+                modifier = Modifier
+                    .background(Color.Gray)
+
+            ) {
+
             }
+            Spacer(modifier = Modifier.height(32.dp))
+            Column(
+                modifier = Modifier
+                    .background(Color.White)
+                    .height(200.dp)
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .padding(end = 20.dp, start = 20.dp)
+            ) {
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    label = { Text(text = "Digite seu email") },
 
-            val tamanhoMaximo = 8
-
-            Spacer(modifier = Modifier.height(48.dp))
-            Card(modifier = Modifier.fillMaxWidth()) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(32.dp)
-                ) {
-
-                    OutlinedTextField(
-                        value = email,
-                        onValueChange = { email = it },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                    trailingIcon = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Default.Email,
+                                contentDescription = ""
+                            )
+                        }
+                    },
+                    isError = emailError,
+                )
+                if (emailError) {
+                    Text(
+                        text = "O email é obrigatório!",
                         modifier = Modifier.fillMaxWidth(),
-                        label = {
-                            Text(text = "email")
-                        },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                        isError = erroEmail
+                        fontSize = 14.sp,
+                        color = Color.Red,
+                        textAlign = TextAlign.End
                     )
-                    if (erroEmail) {
-                        Text(
-                            text = "O e-mail é obrigatório!",
-                            modifier = Modifier.fillMaxWidth(),
-                            fontSize = 14.sp,
-                            color = Color.Red,
-                            textAlign = TextAlign.End
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(10.dp))
-                    OutlinedTextField(
-                        value = password,
-                        onValueChange = {
-                            if (it.length <= tamanhoMaximo) password = it
-                        },
-                        modifier = Modifier.fillMaxWidth(),
-                        label = {
-                            Text(text = "Senha")
-                        },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                        visualTransformation = PasswordVisualTransformation()
-                    )
-                    Spacer(modifier = Modifier.height(32.dp))
-                    Button(
-                        onClick = {
-                            if (email.isEmpty()) {
-                                erroEmail = true
-                            }
-                            erroEmail = false
+                } else {
 
-                        }) {
-                        Text(
-                            text = "ENTRAR",
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .fillMaxWidth(),
-                            textAlign = TextAlign.Center
-                        )
-                    }
                 }
+
+                Spacer(modifier = Modifier.height(10.dp))
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = {
+                        if (it.length <= tamanhoMaximo) password = it
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    label = { Text(text = "Digite sua senha") },
+                    trailingIcon = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            androidx.compose.material3.Icon(
+                                imageVector = Icons.Default.Lock,
+                                contentDescription = ""
+                            )
+                        }
+                    },
+                    visualTransformation = PasswordVisualTransformation(),
+                )
+
+            }
+            //Spacer(modifier = Modifier.height(60.dp))
+            Button(
+                modifier = Modifier.padding(end = 30.dp, start = 30.dp),
+                onClick = {
+                    if (email.isEmpty()) {
+                        emailError = true
+                    }
+
+                }) {
+                Text(
+                    text = "ENTRAR",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
             }
         }
-
     }
 }
+
+
+
+
+
